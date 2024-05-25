3c574300252911edf1fb06cb7e1549fbe0682bff
div40 <vidh40@gmail.com>
2024-05-25 23:28:55 +0530

    page styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,14 @@
 export default function Home() {
-  return <main>Lets Start!</main>;
+  return (
+    <main className="text-center py-32 px-5">
+      <h1 className="text-4xl md:text-5xl font-bold mb-5">
+        Welcome to my blog!
+      </h1>
+      <p className="max-w-3xl mx-auto leading-8">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ea iure
+        quibusdam quis id ratione. Tenetur odio aperiam accusamus amet, ipsam
+        natus porro nam veniam ipsum nihil tempore, est id nulla.
+      </p>
+    </main>
+  );
 }
---
+++ src/app/posts/page.tsx
@@ -0,0 +1,7 @@
+export default function PostPage() {
+  return (
+    <main className="text-center py-32 px-5">
+      <h1 className="text-4xl md:text-5xl font-bold mb-5">List of all Posts</h1>
+    </main>
+  );
+}
